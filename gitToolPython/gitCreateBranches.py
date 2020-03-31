231cf930153fb0d03560fd0d15897246d7b9a019
Andrea Mancini <andrea101288@hotmail.it>
2020-03-31 16:01:52 +0200

    Update get user input in  gitCreateBranches.py

---
+++ gitToolPython/gitCreateBranches.py
@@ -0,0 +1,42 @@
+import git 
+from setting import git_repos, new_branch
+
+class Git_branches:    
+
+    # get user input 
+    def get_user_input(self, input):
+        user_input = input("Insert the "+input+" \n") 
+	return user_input
+
+    # Method to create branches 
+    def create_branches(self, git_repo, user, department, name):
+
+        # define git repo      
+        git_repo = git.Repo(repo)
+        # get remote repo
+        origin = self.git_repo.remote()
+        # get current branch
+        current = self.git_repo.branches['DET_develop']
+        # pull current repo
+        self.git_repo.git.pull('origin', current) 
+        # create branch              
+        self.git_repo.create_head("feature/"+department+name+user)
+        feature = self.git_repo.branches["feature/"+department+name+user]  
+        # push created branch in remote
+        origin.push(new_branch)
+        # checkout in the feature branch 
+        feature.checkout()
+
+
+# Main function					
+if __name__ == "__main__":	
+		
+    # Class instance
+    Branch_creation = Git_branches()
+    # calling Class methods
+    user_name = Branch_creation.get_user_input("user name ex: amancini")
+    department = Branch_creation.get_user_input("department name ex: EU-DET")
+    name = Branch_creation.get_user_input("branch name")
+    for repo in git_repos:
+        Branch_creation.create_branches(repo, user_name, department, name)
+        
--- gitToolPython/gitMergeTool.py
+++ gitToolPython/gitMergeTool.py
@@ -3,46 +3,47 @@ import git
 from setting import git_repos, feature_branches
 
 # Define Class
-class Git_update:	
+class Git_update:
 	
 	# define method to update and merge 2 branches
 	def update_and_merge(self, git_repo, feature_branch, main_branch):
 		#Inizialize git repo
 		repo = git.Repo(git_repo)
 		# get current branch
-		try:
-			current = repo.branches[feature_branch]
-		except:
-			print(feature_branch+" doesn't exist in "+git_repo)
+		if feature_branch in repo.branches:
+			current  =  repo.branches[feature_branch]
+		else:
+			print(feature_branch+" does't exist")
+			quit()			
 		# try to pull from remote feature branch, if the remote branch doesn't exist yet skip this step
 		try:
 			repo.git.pull('origin', current)
 		except:
 			pass
+
 		# get Main branch 
-		try: 
+		if main_branch in repo.branches:
 			main = repo.branches[main_branch]
-		except:
-			print(main_branch+" doesn't exist in "+git_repo)			
-		# git pull from remote main branch
-		repo.git.pull('origin', main)
-		# find the merge base of these two branches
-		base = repo.merge_base(current, main)
-		# stage a merge operation
-		repo.index.merge_tree(main, base=base)
-		# commit changes, providing links to the two parent commits
-		repo.index.commit('Merge '+main_branch+' into '+feature_branch+"'", parent_commits=(current.commit, main.commit))
-		# checkout of the new commit 	 
-		current.checkout(force=True)
-		# download update 
-		repo.git.pull('origin', main)
-		repo.git.pull('origin', current)
-		# push the merged branch to remote, if it doesn't exist yet create it
-		try:
-			repo.git.push(current)
-		except:
-			repo.git.push('--set-upstream', 'origin', current)
-		
+			# git pull from remote main branch
+			repo.git.pull('origin', main)
+			# find the merge base of these two branches
+			base = repo.merge_base(current, main)
+			# stage a merge operation
+			repo.index.merge_tree(main, base=base)
+			# commit changes, providing links to the two parent commits
+			repo.index.commit('Merge '+main_branch+' into '+feature_branch+"'", parent_commits=(current.commit, main.commit))
+			# checkout of the new commit 	 
+			current.checkout(force=True)
+			# download update 
+			repo.git.pull('origin', main)
+			repo.git.pull('origin', current)
+			# push the merged branch to remote, if it doesn't exist yet create it
+			try:
+				repo.git.push(current)
+			except:
+				repo.git.push('--set-upstream', 'origin', current)
+		else:
+			print(main_branch+"doesn't exist")	
 
 # Main function					
 if __name__ == "__main__":
